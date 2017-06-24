cbdf84935929c997aa2f36d00935bcead33a03a4
NNamdi Williams <nnamdiwill@gmail.com>
2017-06-23 23:09:32 -0400

    atmsim
    New class

---
+++ ATMSimulator.java
@@ -0,0 +1,19 @@
+package ATM;
+
+ import java.io.*;
+ import java.util.*;
+ 
+ /** A text based simulation of an ATM
+  * 
+  * @author N'Namdi
+  *
+  */
+public class ATMSimulator {
+	
+	public static void main(String[] args) {
+		
+		ATM theATM;
+	}
+	
+
+}
